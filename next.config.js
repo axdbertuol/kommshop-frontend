97ab24461fce1d20a0dd0f154f7d16dd32717b8f
axdbertuol <axdber@gmail.com>
2023-11-20 07:47:07 -0300

    chore: update

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const withNextIntl = require('next-intl/plugin')()
 
 /** @type {import('next').NextConfig} */
 const nextConfig = withNextIntl({
-  transpilePackages: ['kommshop-types'],
+  // transpilePackages: ['kommshop-types'],
   images: {
     remotePatterns: [
       {
