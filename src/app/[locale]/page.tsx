5748da2da2c66f77e59c957a50fa011599b6a7b7
axdbertuol <axdber@gmail.com>
2024-02-21 21:43:07 -0300

    refactor: remove unnecessary funcs

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -11,11 +11,6 @@ type Props = {
   params: { locale: string }
 }
 export default async function LocalePage({ params: { locale } }: Props) {
-  const cookiesList = await getCookiesList()
-  getEncryptedAuthCookie().then((cookie) => (cookie ? getAuthTokens(cookie) : null))
-  const isValidLocale = locales.some((cur) => cur === locale)
-  if (!isValidLocale) notFound()
-
   // Enable static rendering
   unstable_setRequestLocale(locale)
   redirect('/store')
