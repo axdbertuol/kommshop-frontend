c9cf2f8101395e98919e9591aea977731c7c8a80
axdbertuol <axdber@gmail.com>
2024-02-21 21:44:21 -0300

    refactor: removes imports

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,10 +1,4 @@
-import { notFound, redirect } from 'next/navigation'
-import {
-  getAuthTokens,
-  getCookiesList,
-  getEncryptedAuthCookie,
-} from '../lib/get-cookies-list'
-import { locales } from '../lib/get-locale'
+import { redirect } from 'next/navigation'
 import { unstable_setRequestLocale } from 'next-intl/server'
 
 type Props = {
