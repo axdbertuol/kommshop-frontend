dfc9533e57bbd1eac295e3d188be22a0a1549875
axdbertuol <axdber@gmail.com>
2024-02-25 03:01:30 -0300

    feat: bb img to config

--- next.config.js
+++ next.config.js
@@ -17,6 +17,12 @@ const nextConfig = {
       },
       {
         protocol: 'https',
+        hostname: 'i.ibb.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
         hostname: 'lh3.googleusercontent.com',
         port: '',
         pathname: '/**',
