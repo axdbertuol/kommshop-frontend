d7e8ea8bf40b41904b907b85712b97753c705a81
axdbertuol <axdber@gmail.com>
2024-02-25 03:03:48 -0300

    feat: get-user func

---
+++ src/app/lib/get-user.ts
@@ -0,0 +1,11 @@
+import { getEncryptedAuthCookie, getAuthTokens } from './get-cookies-list'
+
+export async function getUser() {
+  const encryptedAuthCookie = await getEncryptedAuthCookie()
+  let user = null
+  if (encryptedAuthCookie) {
+    const authTokens = await getAuthTokens(encryptedAuthCookie)
+    user = authTokens.user
+  }
+  return user
+}
