ef22bcd45070bd278ffc3ca953a523668e00c461
axdbertuol <axdber@gmail.com>
2024-02-28 03:36:13 -0300

    feat: ssr pages

---
+++ src/app/[locale]/(main)/settings/profile/layout.tsx
@@ -0,0 +1,50 @@
+'use server'
+import { fetchUserProfile } from '@/app/lib/actions/form/get-user-profile'
+import getQueryClient from '@/app/lib/get-query-client'
+import { getUser } from '@/app/lib/get-user'
+import ProfileDetail from '@/components/user/ProfileDetail'
+import { HydrationBoundary, dehydrate } from '@tanstack/react-query'
+import { revalidatePath } from 'next/cache'
+import Image from 'next/image'
+
+import React, { Suspense } from 'react'
+
+const defaultImg = process.env.DEFAULT_USER_IMG_URL ?? ''
+
+export default async function Layout() {
+  // TODO: fetch from user microservice the whole information
+  const user = await getUser()
+  if (!user) return null
+  const queryClient = getQueryClient()
+
+  await queryClient.prefetchQuery({
+    queryKey: ['get-profile', user.id],
+    queryFn: fetchUserProfile,
+  })
+  return (
+    <div className="flex justify-center gap-y-4">
+      <div className="flex h-[70vh] w-[40vw]">
+        <div className="bg-card rounded-lg p-4 shadow-lg ">
+          <Suspense fallback={<>Loading...</>}>
+            <Image
+              src={defaultImg}
+              alt={'pic'}
+              width={300}
+              height={300}
+              className="w-full lg:rounded-tr-none border-primary object-cover rounded-t-lg"
+            />
+          </Suspense>
+        </div>
+        <Suspense fallback={<>Loading...</>}>
+          <HydrationBoundary state={dehydrate(queryClient)}>
+            <ProfileDetail
+              // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+              email={user.email ?? 'noEmailRegistered'}
+              userId={user.id}
+            />
+          </HydrationBoundary>
+        </Suspense>
+      </div>
+    </div>
+  )
+}
--- src/app/[locale]/(main)/settings/profile/page.tsx
+++ src/app/[locale]/(main)/settings/profile/page.tsx
@@ -1,28 +1,3 @@
-import { getEncryptedAuthCookie, getAuthTokens } from '@/app/lib/get-cookies-list'
-import ProfileDetailSegment from '@/components/section-segments/profile-detail-segment'
-import { redirect } from '@/navigation'
-import { Suspense } from 'react'
-
-export default async function Page() {
-  // TODO: fetch from user microservice the whole information
-  const encryptedAuthCookie = await getEncryptedAuthCookie()
-  let user = null
-  if (encryptedAuthCookie) {
-    const authTokens = await getAuthTokens(encryptedAuthCookie)
-    user = authTokens.user
-  }
-  if (!user) {
-    // redirect('/signin')
-    return
-  }
-  return (
-    <Suspense fallback={<>...Loading</>}>
-      <ProfileDetailSegment
-        firstName={user.firstName}
-        lastName={user.lastName}
-        email={user.email}
-        // photo={user?.photo}
-      />
-    </Suspense>
-  )
+export default function Page() {
+  return <></>
 }
