95d383f828a1f14bb43fb2ff5252e135787ccf5d
axdbertuol <axdber@gmail.com>
2024-03-18 23:17:03 -0300

    fix: edit user id check

--- src/app/[locale]/(main)/dashboard/(product-actions)/edit/[slug]/page.tsx
+++ src/app/[locale]/(main)/dashboard/(product-actions)/edit/[slug]/page.tsx
@@ -1,19 +1,19 @@
 import getCategories from '@/app/lib/actions/getters/get-categories'
 import { fetchProduct } from '@/app/lib/actions/getters/get-product'
 import { revalidateProds } from '@/app/lib/cache/revalidators'
-import { getCachedUser } from '@/app/lib/get-user'
+import { getUser } from '@/app/lib/get-user'
 import EditProductForm from '@/components/forms/product/EditProduct'
 import { EditProduct } from '@/types'
 import { notFound } from 'next/navigation'
 
 export default async function Edit({ params: { slug } }: { params: { slug: string } }) {
   if (!slug) notFound()
-  const user = await getCachedUser()
+  const user = await getUser()
   if (!user) return null
   const [categories, product] = await Promise.all([getCategories(), fetchProduct(slug)])
   const { data } = product
   if (!data) return null
-  if (data.ownerId !== user.id) {
+  if (data.owner?.userId !== user.id) {
     return notFound()
   }
   return (
