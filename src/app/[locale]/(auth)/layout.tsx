4856dce45ae96ada8c557f31f0ac3bcba5611348
axdbertuol <axdber@gmail.com>
2024-04-17 11:08:20 -0300

    feat: adds logo

--- src/app/[locale]/(auth)/layout.tsx
+++ src/app/[locale]/(auth)/layout.tsx
@@ -1,5 +1,6 @@
 'use server'
 import { unstable_setRequestLocale } from 'next-intl/server'
+import Image from 'next/image'
 import React from 'react'
 
 type Props = {
@@ -14,6 +15,14 @@ export default async function Layout({ children, params: { locale } }: Props) {
 
   return (
     <div className="min-h-screen pb-4 w-full md:w-[66vw] lg:w-[33vw] bg-primary-foreground dark:bg-neutral-800 md:mx-auto">
+      <div className="w-full pt-4 flex flex-col items-center md:flex md:place-content-center">
+        <Image
+          src={'/kshop.svg'}
+          alt="kommshop"
+          width={110}
+          height={85}
+        />
+      </div>
       {children}
     </div>
   )
--- src/app/[locale]/(main)/layout.tsx
+++ src/app/[locale]/(main)/layout.tsx
@@ -1,11 +1,11 @@
 'use server'
-import React, { Suspense } from 'react'
-import { ReactNode } from 'react'
-import { unstable_setRequestLocale } from 'next-intl/server'
 import { getUser } from '@/app/lib/get-user'
-import { MainNav } from '@/components/nav/MainNav'
 import { ModeToggle } from '@/components/ModeToggle'
+import { MainNav } from '@/components/nav/MainNav'
 import { UserNav } from '@/components/nav/UserNav'
+import { unstable_setRequestLocale } from 'next-intl/server'
+import Image from 'next/image'
+import { ReactNode, Suspense } from 'react'
 
 export default async function Layout({
   children,
@@ -22,7 +22,14 @@ export default async function Layout({
     <>
       <div className="container border-b bg-muted ">
         <div className="flex h-16 ml-16 items-center px-4 gap-2">
-          <h2>Kommshop</h2>
+          <div>
+            <Image
+              src={'/kshop.svg'}
+              alt="kommshop"
+              width={85}
+              height={85}
+            />
+          </div>
           {searchbar}
           <MainNav className="ml-auto w-full" />
           <div className="ml-auto flex items-center space-x-4">
